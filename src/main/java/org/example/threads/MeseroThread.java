1e5958881acdc039a633aeca9e629132361b580b
Aregomz <223728@ids.upchiapas.edu.mx>
2024-11-28 08:57:18 -0600

    mesero.2

--- src/main/java/org/example/threads/MeseroThread.java
+++ src/main/java/org/example/threads/MeseroThread.java
@@ -4,6 +4,8 @@ import org.example.models.MonitorOrdenes;
 import org.example.models.MonitorComidas;
 import org.example.models.Comensal;
 import org.example.models.Orden;
+import org.example.vistas.ClienteVista;
+import org.example.vistas.MeseroVista;
 
 import java.util.List;
 
@@ -11,11 +13,21 @@ public class MeseroThread implements Runnable {
     private MonitorOrdenes monitorOrdenes;
     private MonitorComidas monitorComidas;
     private List<Comensal> comensales;
+    private final List<ClienteVista> listaClientesVista;
+    private MeseroVista meseroVista ;
 
-    public MeseroThread(MonitorOrdenes monitorOrdenes, MonitorComidas monitorComidas, List<Comensal> comensales) {
+    public MeseroThread(
+            MonitorOrdenes monitorOrdenes,
+            MonitorComidas monitorComidas,
+            List<Comensal> comensales,
+            List<ClienteVista> clienteVistaList,
+            MeseroVista mesero
+    ) {
         this.monitorOrdenes = monitorOrdenes;
         this.monitorComidas = monitorComidas;
         this.comensales = comensales;
+        this.listaClientesVista = clienteVistaList;
+        this.meseroVista = mesero;
     }
 
     @Override
@@ -29,9 +41,16 @@ public class MeseroThread implements Runnable {
                             .orElse(null);
 
                     if (comensalAtendido != null) {
+                        ClienteVista clienteActual = this.listaClientesVista.stream()
+                                .filter(vista -> vista.getId() == comensalAtendido.getId())
+                                .findFirst()
+                                .orElse(null);
+
+                        this.meseroVista.moverMesero(comensalAtendido.getMesaId()-1);
                         Orden nuevaOrden = new Orden(comensalAtendido.getMesaId(), comensalAtendido.getId());
                         monitorOrdenes.ponerOrden(nuevaOrden);
                         comensalAtendido.setEstado("Esperando comida");
+
                         System.out.println("Mesero: Tomando orden del comensal " + comensalAtendido.getId());
                     }
                 }
